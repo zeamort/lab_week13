d1bb88d5f2809df3576079c6e9faffd27eef6c27
morteza <zeamort@gmail.com>
2022-03-29 21:20:29 -0700

    delete: added functionality to the clear the copies button to replace the innerHTML content of the copy-container with empty string.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* Clone */
+
 /*Select all divs contained within the original-container class and assign to variable eventDiv*/
 const eventDiv = document.querySelectorAll('.original-container div')
 const copyArea = document.querySelector('.copy-container')
@@ -10,3 +12,9 @@ eventDiv.forEach(element => element.addEventListener('click', (evt) => {
     copyArea.insertAdjacentHTML("beforeend", cloneMaterial);
 }))
 
+/* Delete */
+const resetButton = document.querySelector('button')
+
+resetButton.addEventListener('click', (evt) => {
+    copyArea.innerHTML = '';
+})
