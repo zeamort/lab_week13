d1af2bb1f58766f981a7a771954186b9d847223b
morteza <zeamort@gmail.com>
2022-03-29 20:58:13 -0700

    clone: added js to create click event handlers for the child divs of the original-container, which upon trigger will be copied to the copy-container div.

---
+++ src/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/*Select all divs contained within the original-container class and assign to variable eventDiv*/
+const eventDiv = document.querySelectorAll('.original-container div')
+const copyArea = document.querySelector('.copy-container')
+
+/*forEach function adds event listener to each div in eventDiv */
+eventDiv.forEach(element => element.addEventListener('click', (evt) => {
+    let cloneMaterial = evt.target.parentElement.outerHTML;
+    copyArea.insertAdjacentHTML("beforeend", cloneMaterial);
+}))
+
